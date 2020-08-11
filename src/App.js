8067a069dd82392da5c4d3148c9ccd110f4d3e9c
Manuvant <manutrovant@hotmail.com>
2020-08-10 23:29:35 -0300

    finish file create.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routing from './components/routing';
 import './App.css';
+import CreateProduct from '../src/components/products/create'
 
 function App() {
   return (
     <div className="App">
       <Routing/>
+      <CreateProduct />
     </div>
   );
 }
---
+++ src/conf.js
@@ -0,0 +1,3 @@
+export default {
+    API_URL: 'http://64.225.46.83:3010/api'
+};
