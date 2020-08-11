dea338d1fa64b6943691589c03a642f89c413836
Manuvant <manutrovant@hotmail.com>
2020-08-11 01:30:13 -0300

    Making progress.

--- src/components/products/create.js
+++ src/components/products/create.js
@@ -3,21 +3,34 @@ import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import conf from '../../conf';
+import Snackbar from '@material-ui/core/Snackbar';
+import CircularProgress from '@material-ui/core/CircularProgress'
+import MuiAlert from '@material-ui/lab/Alert';
 
 const styles = {
     textInputMargin: {
         margin: 10
     }
-}
+};
+
+const Alert = (props) => {
+    return (
+        <MuiAlert elevation={6} variant="filled" {...props} />
+    )
+};
+
+const productDefaultValues = {
+    name: '',
+    description: '',
+    price: 0,
+    isImportant: true,
+    created: new Date()
+};
   
 const CreateProduct = (props) => {
-    const [product, setProduct] = React.useState({
-        name: '',
-        description: '',
-        price: 0,
-        isImportant: true,
-        created: new Date()
-    });
+    const [product, setProduct] = React.useState(productDefaultValues);
+    const [snackbarOpen, setSnackbarOpen] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(false);
 
     const handleChange = (evt) => {
         console.log(evt.target.name)
@@ -30,12 +43,26 @@ const CreateProduct = (props) => {
         )
     };
 
+    const handleClose = () => {
+        setSnackbarOpen(
+            false
+        )
+    }
+
     const handleClickUpLoad = () => {
+        setIsLoading(true);
         axios.post(`${conf.API_URL}/products`, product)
                 .then(result => {
+                    setIsLoading(false)
                     console.log(result.data);
+                    setSnackbarOpen(
+                        true
+                    )
+                    
+                    setProduct(productDefaultValues);
                 })
                 .catch(err => {
+                    setIsLoading(false)
                     console.log(err)
                 }); 
     }
@@ -43,14 +70,26 @@ const CreateProduct = (props) => {
     return (
         <div>
             <form noValidate autoComplete="off">
-                <TextField style={styles.textInputMargin} onChange={handleChange} name="name" id="outlined-basic" label="Nombre del Producto" variant="outlined"/>
-                <TextField style={styles.textInputMargin} onChange={handleChange} name="description" id="outlined-basic" label="Descripcion" variant="outlined"/>
-                <TextField style={styles.textInputMargin} onChange={handleChange} name="price" id="outlined-basic" label="Precio" variant="outlined"/>
+                <TextField style={styles.textInputMargin} value={product.name} onChange={handleChange} name="name" id="outlined-basic" label="Nombre del Producto" variant="outlined"/>
+                <TextField style={styles.textInputMargin} value={product.description} onChange={handleChange} name="description" id="outlined-basic" label="Descripcion" variant="outlined"/>
+                <TextField style={styles.textInputMargin} value={product.price} onChange={handleChange} name="price" id="outlined-basic" label="Precio" variant="outlined"/>
             </form>
             <br/>
             <Button onClick={handleClickUpLoad} variant="contained" color="primary" component="span">
-                Upload
+                {
+                    isLoading ? (
+                        <CircularProgress color="white"/>
+                    ) : (
+                        'UPLOAD'
+                    )
+                }
             </Button>
+
+            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    Producto creado
+                </Alert>
+            </Snackbar>
         </div>
     )
 };
