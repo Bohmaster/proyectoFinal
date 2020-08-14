cab361e5e47de26c85ad35638db0c90196a5d939
chilaScript <orefici.diego@gmail.com>
2020-08-14 12:53:49 -0300

    storing de login on localstorage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Typography, Button, makeStyles, TextField, Container, Avatar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Typography, Button, makeStyles, TextField, Container, Avatar, Box } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import conf from '../conf';
+import Axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -17,13 +19,39 @@ const useStyles = makeStyles(theme => ({
         backgroundColor: theme.palette.secondary.main,
     },
     form: {
-        width: '100%', 
+        width: '100%',
         marginTop: theme.spacing(1),
     },
 }))
 
 const Login = () => {
     const classes = useStyles();
+
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: ''
+    })
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        console.log(name, value)
+
+        setCredentials({
+            ...credentials,
+            [name]: value
+        })
+    }
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        Axios.post(`${conf.API_URL}/users/login`, credentials)
+            .then(res => {
+                console.log(res.data);
+                localStorage.setItem('Usuario: ', JSON.stringify(res.data))
+            })
+            .catch(error => console.log('Error: ' + error))
+    }
 
     return (
         <Container component="main" maxWidth="xs">
@@ -34,31 +62,44 @@ const Login = () => {
                 <Typography variant="h6" color="primary">
                     Sign in
                 </Typography>
-                <form className={classes.form}>
-                        <TextField
-                            name="user"
-                            variant="outlined"
-                            placeholder="User*"
-                            required
-                            fullWidth
-                            margin="normal"
-                        />
-                        <TextField
-                            name="password"
-                            type="password"
-                            variant="outlined"
-                            placeholder="Password*"
-                            required
-                            fullWidth
-                            margin="normal"
-                        />
-                </form>
-                <Button 
-                    className={classes.button} 
-                    color="primary" 
-                    variant="contained" 
+                <form onSubmit={onSubmitHandler} className={classes.form}>
+                    <TextField
+                        name="username"
+                        variant="outlined"
+                        placeholder="Username*"
+                        required
+                        fullWidth
+                        margin="normal"
+                        onChange={onChangeHandler}
+                        value={credentials.username}
+                    />
+                    <TextField
+                        name="password"
+                        type="password"
+                        variant="outlined"
+                        placeholder="Password*"
+                        required
+                        fullWidth
+                        margin="normal"
+                        onChange={onChangeHandler}
+                        value={credentials.password}
+                    />
+                    <Button
+                    type="submit"
+                    className={classes.button}
+                    color="primary"
+                    variant="contained"
                     fullWidth>Sign in</Button>
+                </form>
+                
             </div>
+            <Box display="flex" justifyContent="flex-end">
+                <Button 
+                    variant="text" 
+                    color="primary">
+                    Register
+                </Button>
+            </Box>
         </Container >
     )
 }
