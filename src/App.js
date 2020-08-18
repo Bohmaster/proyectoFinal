cee32a39ca095365c8abbb38755f2092e4c6412a
chilaScript <orefici.diego@gmail.com>
2020-08-18 09:58:32 -0300

    adding the router to the main component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './components/temaConfig';
 import Main from './components/Main';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
    return (
       <ThemeProvider theme={theme}>
-         <Main />
+         <Router>
+            <Main />
+         </Router>
       </ThemeProvider>
    );
 }
