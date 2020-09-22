5691b334a23e95e3d806aa38bb4927f690161ebf
Manuvant <manutrovant@hotmail.com>
2020-09-22 12:28:17 -0300

    123

--- src/components/SalesList.js
+++ src/components/SalesList.js
@@ -6,6 +6,9 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import conf from '../conf';
 
 const useStyles = makeStyles((theme) => ({
@@ -13,18 +16,41 @@ const useStyles = makeStyles((theme) => ({
         margin: theme.spacing(1),
         minWidth: 120,
     },
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
     selectEmpty: {
         marginTop: theme.spacing(2),
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
     },
 }))
 
 
 const SalesList = () => {
     const classes = useStyles();
+    const [values, setValues] = React.useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    });
+
 
     const [tipo, setTipo] = useState('');
     const [productList, setProductList] = useState([]);
     const [selectedProductId, setSelectedProductId] = useState(null);
+    const [productosIngresados, setProductosIngresados] = useState([]);
+    const [mostrarListaDeProducto, setMostrarListaDeProducto] = useState(false);
+
+    const handleChangeMonto = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
 
 
     const handleChangeTipo = (event) => {
@@ -43,6 +69,17 @@ const SalesList = () => {
             .catch(err => console.log(err))
     }, [])
 
+    const clickProductosIngresados = (id) => {
+        axios.get(`${conf.API_URL}/products/${id}`)
+            .then(
+                response => {
+                    productosIngresados.push(response)
+                    console.log(productosIngresados)
+                }
+            )
+            .catch(err => console.log(err))
+    };
+    
     return (
         <div>
             <FormControl variant="filled" className={classes.formControl}>
@@ -63,25 +100,59 @@ const SalesList = () => {
             {
                 tipo === 1 ?
                     (
-                        <FormControl variant="filled" className={classes.formControl}>
-                            <InputLabel id="demo-simple-select-filled-label">tipo</InputLabel>
-                            <Select
-                                labelId="demo-simple-select-filled-label"
-                                id="demo-simple-select-filled"
-                                value={selectedProductId}
-                                onChange={handleChangeProduct}
-                            >
-                                <MenuItem value="" disabled>
-                                    <em>----</em>
-                                </MenuItem>
-                                {
-                                    productList.map(product => <MenuItem value={product.id}>{product.name}</MenuItem>)
-                                }
-                            </Select>
-                        </FormControl>
+                        <div>
+                            <FormControl variant="filled" className={classes.formControl}>
+                                <InputLabel id="demo-simple-select-filled-label">tipo</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-filled-label"
+                                    id="demo-simple-select-filled"
+                                    value={selectedProductId}
+                                    onChange={handleChangeProduct}
+                                >
+                                    <MenuItem value="" disabled>
+                                        <em>----</em>
+                                    </MenuItem>
+                                    {
+                                        productList.map(product => <MenuItem onClick={clickProductosIngresados(product.id)} value={product.id}>{product.nombre}1</MenuItem>)
+                                    }
+                                </Select>
+                            </FormControl>
+                        </div>
                     ) :
                     null
             }
+            {
+                tipo === 0 ?
+                    (
+                        <div>
+                            <FormControl fullWidth className={classes.margin} variant="outlined">
+                                <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                                <OutlinedInput
+                                    id="outlined-adornment-amount"
+                                    value={values.amount}
+                                    onChange={handleChangeMonto('amount')}
+                                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                                    labelWidth={60}
+                                />
+                            </FormControl>
+                        </div>
+                    ) :
+                    null
+            }
+            <div>
+                <form className={classes.container} noValidate>
+                    <TextField
+                        id="date"
+                        label="Fecha"
+                        type="date"
+                        defaultValue="2017-05-24"
+                        className={classes.textField}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </form>
+            </div>
         </div>
     )
 };
